9b31f0aa158f66b8ac4508dab759df1dc8c98a63
Bani-Adam <042528546@ecampus.ut.ac.id>
2023-05-14 17:26:46 +0700

    youtubetomp3_web

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import axios from "axios";
+import { useRef, useState } from "react"
+import { youtube_parser } from "./utils";
+
+function App() {
+  const inputUrlRef = useRef();
+  const [urlResult, setUrlResult] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const youtubeID = youtube_parser(inputUrlRef.current.value);
+
+    const options = {
+      method: 'get',
+      url: 'https://youtube-mp36.p.rapidapi.com/dl',
+      headers: {
+        'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY,
+        'X-RapidAPI-Host': 'youtube-mp36.p.rapidapi.com'
+      },
+      params: {
+        id: youtubeID
+      }
+    }
+    axios(options)
+      .then(res => setUrlResult(res.data.link))
+      .catch(err => console.log(err))
+
+    inputUrlRef.current.value = '';
+
+  }
+
+  return (
+    <div className="app">
+        <span className="text">Adam - Project</span>
+      <section className="content">
+        <h1 className="content_title">YouTube to MP3 Converter</h1>
+        <p className="content_description">
+          Ubah video YouTube menjadi MP3 hanya dengan beberapa klik!
+        </p>
+
+        <form onSubmit={handleSubmit} className="form">
+          <input ref={inputUrlRef} placeholder="Paste URL video youtube disini..." className="form_input" type="text" />
+          <button type="submit" className="form_button">Proses</button>
+        </form>
+
+        {urlResult ? <a target='_blank' rel="noreferrer" href={urlResult} className="download_btn">Download MP3</a> : ''}
+        <div className="content_footer">
+        <p><br></br><br></br>
+        <a target='_blank' href="https://baniadam.my.id/mylink"><b>Repositori github</b></a>
+        </p>
+      </div>
+      </section>
+    </div>
+  
+  )
+
+}
+
+export default App
